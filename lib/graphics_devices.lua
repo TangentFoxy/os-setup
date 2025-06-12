b6a5a665acc7c9e8673913d7ec0b29584baa6eed
Rose Liverman <rose@tangentfox.com>
2025-06-12 14:57:14 -0600

    lib to handle #53 gfx device detection

---
+++ lib/graphics_devices.lua
@@ -0,0 +1,45 @@
+-- Returns a table of detected graphics device types using the following names:
+-- {
+--   NVIDIA = true,
+--   AMD = true,
+--   integrated = true,
+--   software = true,
+-- }
+
+local utility = require "lib.utility"
+
+-- while this shouldn't change, since it is possible drivers or settings interfere with accuracy, I'm returning a function instead of the result
+return function()
+  local result = {}
+  local hardware = utility.capture_safe("lspci  -v -s  $(lspci | grep ' VGA ' | cut -d\" \" -f 1)")
+
+  local integrated_graphics_keywords = {
+    "Integrated Graphics", "iGPU", "Intel(R) HD Graphics",
+    "Intel Corporation HD Graphics", "Intel Corporation UHD Graphics",
+  }
+
+  if hardware:find("Software Rendering", 1, true) then
+    result.software = true
+    return result -- there is no graphics hardware at all
+  end
+
+  for _, text in ipairs(integrated_graphics_keywords) do
+    if hardware:find(text, 1, true) then
+      result.integrated = true
+      break
+    end
+  end
+
+  if hardware:find("NVIDIA Corporation", 1, true) then
+    result.NVIDIA = true
+  end
+
+  if hardware:find("AMD/ATI", 1, true) then
+    result.AMD = true
+  end
+
+  -- presence of nvidia-smi in path indicates NVIDIA
+  -- presence of aticonfig  in path indicates AMD
+
+  return result
+end
