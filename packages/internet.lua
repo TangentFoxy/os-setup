0d62065e1223b8b27af85595f5fa5c9e96f8fb47
Rose Liverman <rose@tangentfox.com>
2025-07-13 22:36:05 -0600

    close #94 Vivaldi

--- packages/internet.lua
+++ packages/internet.lua
@@ -1,3 +1,12 @@
 return {
   betterbird = { description = "Betterbird (Thunderbird-based email client, that won't lose your data)", flatpak = "flathub eu.betterbird.Betterbird", },
+  vivaldi = {
+    description = "Vivaldi (chromium-based browser)", binary = true,
+    execute = [[
+      cd ~/Downloads
+      sudo dpkg -i vivaldi*.deb
+      sudo apt-get install -fy
+      rm vivaldi*.deb
+    ]],
+  },
 }
